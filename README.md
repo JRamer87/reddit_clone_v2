efd6e98c9d73fcf12781440cafc04ac40a33cba0
Jon Ramer <jramer87@gmail.com>
2017-04-04 15:04:32 -0500

    updated readme

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Reddit Clone Assessment
 3. Install
   * express
   * KNEX - knex init, configure knexfile.js
+  * PG
   * body-parser
   * morgan
   * ejs
@@ -22,5 +23,7 @@ Reddit Clone Assessment
 10. Create app.js, routes folder, views folder
 11. Create posts, comments, and users folders inside of the views folder
 12. Create all of the views for each resource inside of the corresponding     folders
-13. Require everything in app.js
+13. Create a route file for each resource inside the routes folder
+14. Require everything that you installed from NPM in app.js
+15. Tell your app to use everything that you installed  
 14. Write user routes
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict";
-
+//Templating engine
 const ejs = require('ejs');
+//Makes creation of HTTP servers much easier
 const express = require('express');
+//Gives you acess to express methods suchs as .get, .post, .put etc...
 const app = express();
 const methodOverride = require('method-override');
 const morgan = require('morgan');
@@ -10,22 +12,27 @@ const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const commentsRouter = require('./routes/comments');
 
+//Sets the view engine to ejs which looks in the views folder by default
 app.set('view engine', 'ejs');
-//app.use(express.static(__dirname + '/public'));//What is this doing?
+//Logs the type of request and route as it hits the server
 app.use(morgan("tiny"));
+//Parses incoming request bodies in a middleware before your handlers, available under the req.body property.
 app.use(bodyParser.urlencoded({
-    extended: true
+    extended: true //A new body object containing the parsed data is populated on the request object after the middleware (i.e. req.body). This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
 }));
+//Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 app.use(methodOverride("_method"));
-
+//Renders the home page from the statics folder in views
 app.get("/", (req, res) => {
     res.render("statics/home");
 });
-
+//Sets the router to usersRouter anytime a url starts with /users
 app.use("/users", usersRouter);
+//Sets the router to postsRouter anytime a url starts with /posts
 app.use("/posts", postsRouter);
+//Sets the router to commentsRouter anytime a url starts with /comments
 app.use("/comments", commentsRouter);
-
+//Tells the server to listen on the process.env port or 3000 if no process.env.PORT is provided also logs "Listening on port 3000" to the server
 app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000");
 });
